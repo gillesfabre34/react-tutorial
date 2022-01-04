9dd954ffdfee7fb6f6c0d52d679dc7d508f82884
Gilles Fabre <gillesfabre34@gmail.com>
2022-01-04 16:29:12 +0100

    Adding temperature component

--- src/components/temperature.tsx
+++ src/components/temperature.tsx
@@ -7,6 +7,8 @@ export function isCelsius(scale: TempScale): boolean {
 }
 
 export interface ITemperature {
+    celsius: string,
+    fahrenheit: string,
     message: string
 }
 
@@ -17,7 +19,7 @@ export class Temperature extends React.Component<{}, ITemperature> {
 
     constructor(props: any) {
         super(props);
-        this.state = {message: this.boilsMsg}
+        this.state = {message: this.boilsMsg, celsius: '', fahrenheit: ''}
         this.refresh = this.refresh.bind(this);
         console.log("SATTE", this.state)
     }
@@ -26,9 +28,11 @@ export class Temperature extends React.Component<{}, ITemperature> {
         const scale: TempScale = e.target.name;
         const temperature: number = e.target.value;
         console.log("refreshhh", e.target.value, e.target.name)
-        const convertedTemperature: number = this.convert(scale, temperature);
-        console.log('conv', convertedTemperature);
-        this.setState({message: this.isBoiling(scale, temperature) ? this.boilsMsg : this.notBoilsMsg});
+        const celsius: number = isCelsius(scale) ? temperature : this.convert(scale, temperature);
+        const fahrenheit: number = this.convert('Celsius', celsius);
+        const message: string = this.isBoiling(scale, temperature) ? this.boilsMsg : this.notBoilsMsg;
+        this.setState({message, fahrenheit: fahrenheit.toString(), celsius: celsius.toString()});
+        console.log("NEW STATE", this.state)
     }
 
     isBoiling(scale: TempScale, value: number): boolean {
@@ -45,11 +49,11 @@ export class Temperature extends React.Component<{}, ITemperature> {
             <>
                 <div style={{margin: "2em"}}>
                     <label htmlFor="Celsius">T (°C)</label>
-                    <input type="text" id="Celsius" name="Celsius" onKeyUp={this.refresh}/>
+                    <input type="text" id="Celsius" name="Celsius" defaultValue={this.state.celsius} onKeyUp={this.refresh}/>
                 </div>
                 <div style={{margin: "2em"}}>
                     <label htmlFor="Fahrenheit">T (°F)</label>
-                    <input type="text" id="Fahrenheit" name="Fahrenheit"/>
+                    <input type="text" id="Fahrenheit" name="Fahrenheit" defaultValue={this.state.fahrenheit} onKeyUp={this.refresh}/>
                 </div>
                 <div>{this.state.message}</div>
             </>
