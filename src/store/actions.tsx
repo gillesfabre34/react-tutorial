7a7c2037de0a6e3235a861b0d1cad92f25ef0d5f
Gilles Fabre <gillesfabre34@gmail.com>
2022-01-21 08:30:57 +0100

    Added createAsyncThunk

--- src/store/actions.tsx
+++ src/store/actions.tsx
@@ -2,9 +2,4 @@ export const CREATE_PRODUCT = 'products/createProduct';
 export const WRONG_DATA = 'products/wrongProductData';
 export const PRODUCT_ALREADY_EXISTS = 'products/productAlreadyExists';
 export const USERS_LOADED = 'users';
-
-export const getUsers = async (dispatch) => {
-	const response = await fetch("https://jsonplaceholder.typicode.com/users");
-	const json = await response.json();
-	dispatch({type: USERS_LOADED, payload: json});
-}
+export const GET_USERS = 'users/get';
--- src/store/slices/usersSlice.ts
+++ src/store/slices/usersSlice.ts
@@ -1,6 +1,7 @@
-import { createSlice } from '@reduxjs/toolkit';
+import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
 import { User } from '../../components/users';
 import { RootState } from '../store';
+import { GET_USERS } from '../actions';
 
 export interface UsersState {
 	users: User[];
@@ -10,17 +11,25 @@ const initialState: UsersState = {
 	users: [],
 }
 
+const getUsersThunk = async (data: any) => {
+	const response = await fetch("https://jsonplaceholder.typicode.com/users");
+	return await response.json();
+}
+
+export const getUsers = createAsyncThunk(GET_USERS, getUsersThunk);
+
 const usersSlice = createSlice({
 	name: 'users',
 	initialState,
-	reducers: {
-		getUsers: (state, action) => {
-			return {...state, users: []};
-		}
+	reducers: {},
+	extraReducers: builder => {
+		builder.addCase(getUsers.fulfilled, (state: any, action) => {
+			console.log("EXTRA action", action);
+			state.users = action.payload;
+		})
 	}
 })
 
 export const selectUsers = (state: RootState) => state.usersReducer.users;
 
-export const { getUsers } = usersSlice.actions;
 export const usersReducer = usersSlice.reducer;
