769e5cbd39fea19fe0dc3bc2ed33a0a7550efc67
Gilles Fabre <gillesfabre34@gmail.com>
2022-01-21 13:49:34 +0100

    Eslint fails

---
+++ .eslintrc.js
@@ -0,0 +1,9 @@
+module.exports = {
+  root: true,
+  extends: ["react-app", "eslint:recommended"],
+  parser: '@typescript-eslint/parser',
+  plugins: ['@typescript-eslint'],
+  rules: {
+    curly: 0
+  }
+};
---
+++ .prettierrc.js
@@ -0,0 +1,8 @@
+module.exports = {
+  bracketSpacing: true,
+  jsxBracketSameLine: true,
+  singleQuote: true,
+  endOfLine: 'auto',
+  tabWidth: 2,
+  printWidth: 100,
+};
